241b27e8fefa31db45de33634f255bf03673eb5e
TanlnWZaa007 <153242404+TanlnWZaa007@users.noreply.github.com>
2024-12-20 17:56:44 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+    
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));  
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};  
+    int x = rand() % 9;  
+
+    cout << "You will get " << grade[x] << " in this 261102." << endl;
+
+    return 0;
+}
+
